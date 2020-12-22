c49f0b41e6ac1ac2244f3e1755a4dc924d14f49a
Aaron Lilly <38325891+aaronlilly@users.noreply.github.com>
2020-12-21 19:38:13 -0500

    Update basic.js

--- scripts/basic.js
+++ scripts/basic.js
@@ -25,7 +25,7 @@ function checkCookie() {
   var user=getCookie("username");
   if (user != "") {
     alert("Welcome again " + user);
-  } else if (user === "undefined"){
+  } else if (user === undefined){
      user = prompt("Please enter your name:","");
      if (user != "" && user != null) {
        setCookie("username", user, 30);
